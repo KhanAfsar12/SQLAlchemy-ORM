1efe3a84ff7b0fbcd3759d5d03c5cf400a38ff03
KhanAfsar12 <ka484564@gmail.com>
2024-03-02 15:55:09 +0530

    retriving by ascending & descending

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from models import User, engine
-import random
+
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Add random data
-names = ["Ishtiyaque", "Nawaz", "Farman", "sadique"]
-ages = [12,45,32,54,23,76,44,34,9]
-for x in range(20):
-    user = User(name=random.choice(names), age=random.choice(ages))
-    session.add(user)
-session.commit()
+# retriving by ascending & descending
+users = session.query(User).order_by(User.name, User.age).all()
+for user in users:
+    print(f"User age: {user.age}, name: {user.name}, id: {user.id}")
