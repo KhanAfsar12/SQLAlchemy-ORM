e7e64aa4bbbb8c30a96bdc17f67877062886b261
KhanAfsar12 <ka484564@gmail.com>
2024-03-02 15:44:15 +0530

    Add random data

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from models import User, engine
+import random
 
 Session = sessionmaker(bind=engine)
 session = Session()
-# Inserting data into talble
-# user = User(name="Afsar Khan", age=21)
-# session.add(user)
-# session.commit()
-# session.close()
 
-
-# Reading data
-# users = session.query(User).all()
-# for user in  users:
-#     print(f"User Id:{user.id}  name:{user.name}  age:{user.age}")
-
-
-# Updating data
-# user = session.query(User).filter_by(id=2).one_or_none()
-# user.name = "Abrar Khan"
-# print(user.name)
-# session.commit()
-
-
-# delete data
-user = session.query(User).filter_by(id=4).one_or_none()
-session.delete(user)
+# Add random data
+names = ["Ishtiyaque", "Nawaz", "Farman", "sadique"]
+ages = [12,45,32,54,23,76,44,34,9]
+for x in range(20):
+    user = User(name=random.choice(names), age=random.choice(ages))
+    session.add(user)
 session.commit()
